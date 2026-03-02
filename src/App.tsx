4a70d96dd58caf18ec7cb1323d87dadf3facae9b
fabio araujo <fabio.araujo@conecthus.org.br>
2026-03-02 11:14:06 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500">
+        JailTracker Frontend rodando!
+      </h1>
+    </div>
+  )
+}
+
+export default App
