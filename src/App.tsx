6afd66589c6ec0a83d5542482b5da57bab18541a
Kevin Marques <kevinsilvacontact2023@gmail.com>
2026-03-05 15:23:58 -0400

    Removendo importação do Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import DashBoardPage from './pages/DashBoardPage';
 
