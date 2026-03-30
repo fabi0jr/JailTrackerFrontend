3924179a88e40b51de6958b6f02c4c770df0e350
Kevin <kevinmarques2018@gmail.com>
2026-03-30 15:53:41 -0300

    Feita a integração com o backend

--- src/pages/RegisterPrisoner.tsx
+++ src/pages/RegisterPrisoner.tsx
@@ -1,33 +1,97 @@
 import { PlusCircle, Search, Trash2, UserPlus, Users } from "lucide-react";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom";
+import LoadingModel from "../components/LoadingModel";
 
 //TIPAGEM DOS DADOS MOCKADOS:
 interface Prisoner {
-    id: number;
     nome: string;
-    nomeMae: string;
     cpf: string;
     delito: string;
-    reincidencia: boolean;
-    dataNasc: string;
     pavilhao: string;
     cela: string;
     foto: string;
-    confinament: boolean
+    naSolitaria: boolean
+    id: number
 }
 
 export default function RegisterPrisoner() {
+    const BACKEND_API = import.meta.env.VITE_BACKEND_API
+    const navigate = useNavigate();
+    const [prisoners, setPrisoners] = useState<Prisoner[]>([]);
+    const [isLoadingPrisoners, setIsLoadingPrisoners] = useState(true);
 
     //DADOS MOCKADOS:
-    const [prisoners] = useState<Prisoner[]>([
-        { id: 1, nome: "Fabio Junior", nomeMae: "Maria Silva", cpf: "911.304.320-09", delito: "Roubo", reincidencia: true, dataNasc: "01/01/1990", pavilhao: "A", cela: "Cela 1", foto: "https://randomuser.me/api/portraits/men/32.jpg", confinament: true },
-        { id: 2, nome: "Abinadabe Fontenele Sodre", nomeMae: "Ana Sodre", cpf: "123.456.789-10", delito: "Homicídio", reincidencia: false, dataNasc: "15/05/1985", pavilhao: "A", cela: "Solitária", foto: "https://randomuser.me/api/portraits/men/85.jpg", confinament: false },
-        { id: 3, nome: "Kevin Marques", nomeMae: "Clara Marques", cpf: "348.678.901-22", delito: "Tráfico", reincidencia: true, dataNasc: "20/10/1995", pavilhao: "B", cela: "Cela 3", foto: "https://randomuser.me/api/portraits/men/22.jpg", confinament: false },
-        { id: 4, nome: "Marcos Vinicius", nomeMae: "Beatriz Santos", cpf: "555.666.777-88", delito: "Furto", reincidencia: false, dataNasc: "05/12/1998", pavilhao: "C", cela: "Cela 12", foto: "https://randomuser.me/api/portraits/men/55.jpg", confinament: false },
-    ]);
+    // const [prisoners] = useState<Prisoner[]>([
+    //     { id: 1, nome: "Fabio Junior", nomeMae: "Maria Silva", cpf: "911.304.320-09", delito: "Roubo", reincidencia: true, dataNasc: "01/01/1990", pavilhao: "A", cela: "Cela 1", foto: "https://randomuser.me/api/portraits/men/32.jpg", confinament: true },
+    //     { id: 2, nome: "Abinadabe Fontenele Sodre", nomeMae: "Ana Sodre", cpf: "123.456.789-10", delito: "Homicídio", reincidencia: false, dataNasc: "15/05/1985", pavilhao: "A", cela: "Solitária", foto: "https://randomuser.me/api/portraits/men/85.jpg", confinament: false },
+    //     { id: 3, nome: "Kevin Marques", nomeMae: "Clara Marques", cpf: "348.678.901-22", delito: "Tráfico", reincidencia: true, dataNasc: "20/10/1995", pavilhao: "B", cela: "Cela 3", foto: "https://randomuser.me/api/portraits/men/22.jpg", confinament: false },
+    //     { id: 4, nome: "Marcos Vinicius", nomeMae: "Beatriz Santos", cpf: "555.666.777-88", delito: "Furto", reincidencia: false, dataNasc: "05/12/1998", pavilhao: "C", cela: "Cela 12", foto: "https://randomuser.me/api/portraits/men/55.jpg", confinament: false },
+    // ]);
 
 
+
+    useEffect(() => {
+        const fetchWithRefresh = async (endpoint: string) => {
+            const token = Cookies.get('access_token');
+
+            let response = await fetch(`${BACKEND_API}${endpoint}`, {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+
+            if (response.status === 401) {
+                console.log(`Token expirado ao acessar ${endpoint}. Tentando refresh...`);
+                const refreshToken = Cookies.get('refresh_token');
+                if (!refreshToken) throw new Error("Sem refresh token disponível");
+
+                const refreshRes = await fetch(`${BACKEND_API}/auth/refresh`, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${refreshToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                if (refreshRes.ok) {
+                    const data = await refreshRes.json();
+
+                    const newToken = data.access_token;
+
+                    Cookies.set('access_token', newToken, { expires: 1, path: '/' });
+
+                    response = await fetch(`${BACKEND_API}${endpoint}`, {
+                        headers: { 'Authorization': `Bearer ${newToken}` }
+                    });
+                } else {
+                    console.error("Refresh token expirado. Redirecionando para login.");
+                    Cookies.remove('access_token', { path: '/' });
+                    Cookies.remove('refresh_token', { path: '/' });
+                    navigate("/");
+                    return null;
+                }
+            }
+            return response.ok ? await response.json() : null;
+        };
+        const fetchData = () => {
+            try {
+                fetchWithRefresh('/prisoners').then((valueData) => {
+                    if (valueData) {
+                        setPrisoners(valueData)
+                        setIsLoadingPrisoners(false)
+                        console.log(valueData)
+                    }
+                })
+
+            } catch (error: unknown) {
+                console.error("Erro crítico na integração:", error instanceof Error ? error.message : error);
+            }
+        };
+
+        fetchData();
+
+    }, [])
     return (
         <div className="h-screen flex flex-col bg-gray-100 font-sans text-slate-900 overflow-hidden">
             <Header page="registerPrisorners" />
@@ -75,45 +139,47 @@ export default function RegisterPrisoner() {
                         <Users size={20} className="text-gray-400" /> Lista de Presos Cadastrados
                     </h2>
 
-                    <div className="flex-1 overflow-y-auto pr-2 space-y-4 custom-scrollbar">
-                        {prisoners.map((p) => (
-                            <div key={p.id} className="bg-gray-50 rounded-xl p-5 border border-gray-100 flex items-center justify-between transition-colors hover:border-gray-200 hover:bg-white group">
-                                <div className="flex items-center gap-4">
-                                    <img
-                                        src={p.foto}
-                                        alt={p.nome}
-                                        className="w-14 h-14 rounded-full border-2 border-white ring-2 ring-gray-100 shadow-inner object-cover shrink-0"
-                                    />
+                    {
+                        isLoadingPrisoners ? <LoadingModel /> : <div className="flex-1 overflow-y-auto pr-2 space-y-4 custom-scrollbar">
+                            {prisoners.map((p) => (
+                                <div key={p.id} className="bg-gray-50 rounded-xl p-5 border border-gray-100 flex items-center justify-between transition-colors hover:border-gray-200 hover:bg-white group">
+                                    <div className="flex items-center gap-4">
+                                        <img
+                                            src={p.foto != null ? p.foto : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg8keaWuTWemET3-1mWqZae05N8W6SLGgGg&s"}
+                                            alt={p.nome}
+                                            className="w-14 h-14 rounded-full border-2 border-white ring-2 ring-gray-100 shadow-inner object-cover shrink-0"
+                                        />
 
-                                    <div>
-                                        <h3 className="font-bold text-base text-slate-800">{p.nome}</h3>
-                                        <p className="text-xs text-gray-400 font-medium">CPF: {p.cpf}</p>
-                                    </div>
-                                </div>
-
-                                <div className="flex items-center gap-10 text-right">
-                                    <div className="grid grid-cols-2 gap-x-8 gap-y-1 text-sm">
-                                        <p className="font-medium text-gray-600">Pavilhão: <span className="font-bold text-slate-900">{p.pavilhao}</span></p>
-                                        <p className="font-medium text-gray-600">Cela: <span className="font-bold text-slate-900">{p.cela}</span></p>
-                                        <p className="font-medium text-gray-600">Delito: <span className="font-bold text-red-600">{p.delito}</span></p>
-                                        <p className="font-medium text-gray-600">Situação: <span className="font-bold text-red-600">{p.confinament ? "Solitária" : "Normal"}</span></p>
-                                    </div>
-
-                                    <button className="hover:bg-gray-200 cursor-pointer px-3 py-2 rounded-2xl">
-                                        <div className="flex gap-2">
-                                            <Search className="text-black" size={18} />
-                                            <div>Ver detalhes</div>
+                                        <div>
+                                            <h3 className="font-bold text-base text-slate-800">{p.nome}</h3>
+                                            <p className="text-xs text-gray-400 font-medium">CPF: {p.cpf}</p>
                                         </div>
-                                    </button>
+                                    </div>
+
+                                    <div className="flex items-center gap-10 text-right">
+                                        <div className="grid grid-cols-2 gap-x-8 gap-y-1 text-sm">
+                                            <p className="font-medium text-gray-600">Pavilhão: <span className="font-bold text-slate-900">{p.pavilhao}</span></p>
+                                            <p className="font-medium text-gray-600">Cela: <span className="font-bold text-slate-900">{p.cela}</span></p>
+                                            <p className="font-medium text-gray-600">Delito: <span className="font-bold text-red-600">{p.delito}</span></p>
+                                            <p className="font-medium text-gray-600">Situação: <span className="font-bold text-red-600">{p.naSolitaria ? "Solitária" : "Normal"}</span></p>
+                                        </div>
+
+                                        <button className="hover:bg-gray-200 cursor-pointer px-3 py-2 rounded-2xl">
+                                            <div className="flex gap-2">
+                                                <Search className="text-black" size={18} />
+                                                <div>Ver detalhes</div>
+                                            </div>
+                                        </button>
 
 
-                                    <button className="text-gray-400 opacity-0 group-hover:opacity-100 group-hover:text-red-600 transition-all p-2 rounded-lg hover:bg-red-50">
-                                        <Trash2 size={18} />
-                                    </button>
+                                        <button className="text-gray-400 opacity-0 group-hover:opacity-100 group-hover:text-red-600 transition-all p-2 rounded-lg hover:bg-red-50">
+                                            <Trash2 size={18} />
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    }
                 </section>
             </main>
         </div>
